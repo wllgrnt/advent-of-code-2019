b877eabfc3359be9e1b83293ccc8a2fe90bf6de7
Will Grant <wpg23@cam.ac.uk>
2019-12-02 14:52:19 -0800

    Part Two of Day 1

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -14,7 +14,7 @@ fn run(filename: &str) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(filename)?;
 
     let lines = contents.lines();
-    let mut input_data = vec![];
+    let input_data = &mut vec![];
     for line in lines {
         input_data.push(line.trim().parse::<i64>()?);
     }
@@ -23,13 +23,40 @@ fn run(filename: &str) -> Result<(), Box<dyn Error>> {
     // then sum
     let total_fuel_required = input_data
         .into_iter()
-        .fold(0, |acc, mass| acc + fuel_from_mass(mass));
+        .fold(0, |acc, mass| acc + fuel_from_mass(*mass));
 
     println!("Total fuel required: {}", total_fuel_required);
+
+    // Part two: fuel itself has mass, so needs fuel
+
+    let total_fuel_required_refined = input_data.into_iter().fold(0, |acc, mass| {
+        acc + fuel_from_mass_with_fuel_mass_included(*mass)
+    });
+
+    println!(
+        "Total fuel required (refined): {}",
+        total_fuel_required_refined
+    );
+
     Ok(())
 }
 
 fn fuel_from_mass(mass: i64) -> i64 {
     let fuel = mass / 3 - 2;
     fuel
+}
+
+fn fuel_from_mass_with_fuel_mass_included(mass: i64) -> i64 {
+    // Calculate the fuel required for the module
+    let mut required_fuel = fuel_from_mass(mass);
+    // Then treat the fuel amount as the input mass, and repeat, until the
+    // fuel amount is zero or negative
+    let mut remaining_fuel = required_fuel;
+    while remaining_fuel >= 0 {
+        remaining_fuel = fuel_from_mass(remaining_fuel);
+        if remaining_fuel > 0 {
+            required_fuel += remaining_fuel;
+        }
+    }
+    required_fuel
 }
