4e44d2de72cdb11b50d5ec5e758611d97a37c699
Will Grant <wpg23@cam.ac.uk>
2019-12-03 12:11:03 -0800

    Janky solution to day 3 using lots of match

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -26,27 +26,27 @@ fn run(filename: &str) -> Result<(), Box<dyn Error>> {
 
     // For every segment in the second wire, see if a segment in the first wire crosses it.
 
+    let mut nearest_crossing: [i32; 2] = [1000000000, 1000000000];
+
     for first_segment in &first_wire_segments {
-        let first_wire_segment_start = first_segment.start;
-        let first_wire_segment_end = first_segment.end;
-
         for second_segment in &second_wire_segments {
-            let second_wire_segment_start = second_segment.start;
-            let second_wire_segment_end = second_segment.end;
-
             // do these two lines cross
-            let line_crossing = do_lines_cross(first_segment, second_segment);
-            if line_crossing {
-                println!("{:?}", first_wire_segment_start);
-                println!("{:?}", first_wire_segment_end);
-                println!("{:?}", second_wire_segment_start);
-                println!("{:?}", second_wire_segment_end);
+            let line_crossing: Option<[i32; 2]> = do_lines_cross(first_segment, second_segment);
+
+            match line_crossing {
+                None => (),
+                Some(crossing) => {
+                    if crossing[0].abs() + crossing[1].abs()
+                        < nearest_crossing[0].abs() + nearest_crossing[1].abs()
+                    {
+                        nearest_crossing = [crossing[0], crossing[1]];
+                    }
+                }
             }
         }
     }
-
-    println!("{:?}", first_wire);
-    println!("{:?}", first_wire_segments);
+    println!("{:?}", nearest_crossing);
+    // println!("{:?}", first_wire_segments);
     // println!("{:?}",  second_wire);
 
     Ok(())
@@ -83,11 +83,44 @@ fn parse_wire_stringarray_to_segment_list(wire_string_array: &Vec<&str>) -> Vec<
     wire_coords
 }
 
-fn do_lines_cross(first_wire_segment: &WireSegment, second_wire_segment: &WireSegment) -> bool {
+fn do_lines_cross(
+    first_wire_segment: &WireSegment,
+    second_wire_segment: &WireSegment,
+) -> Option<[i32; 2]> {
     // Check whether the two lines cross
     // Two conditions: they're either parallel, or perpendicular
+    // If they're parallel, they meet if there respective co-ordinates overlap. Then choose the point
+    // With the lowest value.
+    // If they're perpendicular, they cross if A_X1 <= B_X1 <= B_X2 and B_Y1 <= A_Y1 <= B_Y2
+    // When B is vertical, A is horizontal, and the points are (A_X1, A_Y1) and (A_X2, A_Y2) etc.
+    if lines_parallel(first_wire_segment, second_wire_segment) {
+        // Ignore this case for now
+        None
+    } else {
+        let (a_x1, a_x2, a_y1, _a_y2, b_x1, _b_x2, b_y1, b_y2) =
+            return_ordered_co_ords(first_wire_segment, second_wire_segment);
 
-    false
+        // cross if a_x1 <= b_x1 <= a_x2 and b_y1 <= a_y1 <= b_y2. then the cross is at b_x1, a_y1
+        if (a_x1 <= b_x1) && (b_x1 <= a_x2) && (b_y1 <= a_y1) && (a_y1 <= b_y2) {
+            Some([b_x1, a_y1])
+        } else {
+            None
+        }
+    }
+}
+
+fn lines_parallel(first_wire_segment: &WireSegment, second_wire_segment: &WireSegment) -> bool {
+    if (first_wire_segment.direction == "U" || first_wire_segment.direction == "D")
+        && (second_wire_segment.direction == "U" || second_wire_segment.direction == "D")
+    {
+        true
+    } else if (first_wire_segment.direction == "L" || first_wire_segment.direction == "R")
+        && (second_wire_segment.direction == "L" || second_wire_segment.direction == "R")
+    {
+        true
+    } else {
+        false
+    }
 }
 
 #[derive(Debug)]
@@ -95,4 +128,123 @@ struct WireSegment {
     start: [i32; 2],
     end: [i32; 2],
     direction: String,
+}
+
+fn return_ordered_co_ords(
+    first_wire_segment: &WireSegment,
+    second_wire_segment: &WireSegment,
+) -> (i32, i32, i32, i32, i32, i32, i32, i32) {
+    let (mut a_x1, mut a_x2, mut a_y1, mut a_y2) = (-1, -1, -1, -1);
+    let (mut b_x1, mut b_x2, mut b_y1, mut b_y2) = (-1, -1, -1, -1);
+
+    match first_wire_segment.direction.as_str() {
+        "R" => {
+            a_x1 = first_wire_segment.start[0];
+            a_x2 = first_wire_segment.end[0];
+            a_y1 = first_wire_segment.start[1];
+            a_y2 = first_wire_segment.end[1];
+            assert_eq!(a_y1, a_y2);
+            match second_wire_segment.direction.as_str() {
+                "U" => {
+                    b_x1 = second_wire_segment.start[0];
+                    b_x2 = second_wire_segment.end[0];
+                    b_y1 = second_wire_segment.start[1];
+                    b_y2 = second_wire_segment.end[1];
+                    assert_eq!(b_x1, b_x2);
+                }
+                "D" => {
+                    b_x2 = second_wire_segment.start[0];
+                    b_x1 = second_wire_segment.end[0];
+                    b_y2 = second_wire_segment.start[1];
+                    b_y1 = second_wire_segment.end[1];
+                    assert_eq!(b_x1, b_x2);
+                }
+                _ => (),
+            }
+        }
+        "L" => {
+            a_x2 = first_wire_segment.start[0];
+            a_x1 = first_wire_segment.end[0];
+            a_y2 = first_wire_segment.start[1];
+            a_y1 = first_wire_segment.end[1];
+            assert_eq!(a_y1, a_y2);
+            match second_wire_segment.direction.as_str() {
+                "U" => {
+                    b_x1 = second_wire_segment.start[0];
+                    b_x2 = second_wire_segment.end[0];
+                    b_y1 = second_wire_segment.start[1];
+                    b_y2 = second_wire_segment.end[1];
+                    assert_eq!(b_x1, b_x2);
+                }
+                "D" => {
+                    b_x2 = second_wire_segment.start[0];
+                    b_x1 = second_wire_segment.end[0];
+                    b_y2 = second_wire_segment.start[1];
+                    b_y1 = second_wire_segment.end[1];
+                    assert_eq!(b_x1, b_x2);
+                }
+                _ => (),
+            }
+        }
+        "U" => {
+            b_x1 = first_wire_segment.start[0];
+            b_x2 = first_wire_segment.end[0];
+            b_y1 = first_wire_segment.start[1];
+            b_y2 = first_wire_segment.end[1];
+            assert_eq!(b_x1, b_x2);
+            match second_wire_segment.direction.as_str() {
+                "L" => {
+                    a_x2 = second_wire_segment.start[0];
+                    a_x1 = second_wire_segment.end[0];
+                    a_y2 = second_wire_segment.start[1];
+                    a_y1 = second_wire_segment.end[1];
+                    assert_eq!(a_y1, a_y2);
+                }
+                "R" => {
+                    a_x1 = second_wire_segment.start[0];
+                    a_x2 = second_wire_segment.end[0];
+                    a_y1 = second_wire_segment.start[1];
+                    a_y2 = second_wire_segment.end[1];
+                    assert_eq!(a_y1, a_y2);
+                }
+                _ => (),
+            }
+        }
+        "D" => {
+            b_x2 = first_wire_segment.start[0];
+            b_x1 = first_wire_segment.end[0];
+            b_y2 = first_wire_segment.start[1];
+            b_y1 = first_wire_segment.end[1];
+            assert_eq!(b_x1, b_x2);
+            match second_wire_segment.direction.as_str() {
+                "L" => {
+                    a_x2 = second_wire_segment.start[0];
+                    a_x1 = second_wire_segment.end[0];
+                    a_y2 = second_wire_segment.start[1];
+                    a_y1 = second_wire_segment.end[1];
+                    assert_eq!(a_y1, a_y2);
+                }
+                "R" => {
+                    a_x1 = second_wire_segment.start[0];
+                    a_x2 = second_wire_segment.end[0];
+                    a_y1 = second_wire_segment.start[1];
+                    a_y2 = second_wire_segment.end[1];
+                    assert_eq!(a_y1, a_y2);
+                }
+                _ => (),
+            }
+        }
+        _ => (),
+    }
+
+    assert_ne!(a_x1, -1);
+    assert_ne!(a_x2, -1);
+    assert_ne!(a_y1, -1);
+    assert_ne!(a_y2, -1);
+    assert_ne!(b_x1, -1);
+    assert_ne!(b_x2, -1);
+    assert_ne!(b_y1, -1);
+    assert_ne!(b_y2, -1);
+
+    (a_x1, a_x2, a_y1, a_y2, b_x1, b_x2, b_y1, b_y2)
 }
