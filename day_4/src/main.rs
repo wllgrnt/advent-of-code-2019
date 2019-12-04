475efa045180a88ea5442db155006d0d0c55101f
Will Grant <wpg23@cam.ac.uk>
2019-12-04 10:56:32 -0800

    updated to solve part 2

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -17,16 +17,29 @@ fn run(password_range: [i32; 2]) -> Result<(), Box<dyn Error>> {
     - Going from left to right, the digits never decrease
 
     How many passwords meet this criteria?
+
+    Part 2:
+    - the two adjacent matching digits are not part of a larger group of matching digits.
+    (i.e the set of double digits occurs exactly twice)
+    });
     */
-    let mut num_passwords = 0;
+    let mut num_passwords_part_one = 0;
+    let mut num_passwords_part_two = 0;
     for candidate_password in password_range[0]..password_range[1] {
         // Get the digits in order
         let digits = get_digits(candidate_password);
-        if is_valid(digits) {
-            num_passwords += 1;
+        if is_valid_part_one(digits.clone()) {
+            num_passwords_part_one += 1;
+        }
+        if is_valid_part_two(digits.clone()) {
+            num_passwords_part_two += 1;
         }
     }
-    println!("Num of valid passwords: {}", num_passwords);
+    println!("Num of valid passwords: {}", num_passwords_part_one);
+    println!(
+        "Num of valid passwords with extra criterion: {}",
+        num_passwords_part_two
+    );
     Ok(())
 }
 
@@ -42,7 +55,7 @@ fn get_digits(n: i32) -> Vec<i32> {
     xs
 }
 
-fn is_valid(digits: Vec<i32>) -> bool {
+fn is_valid_part_one(digits: Vec<i32>) -> bool {
     // Check that at least two digits are same
     // And the digits never decrease
 
@@ -61,3 +74,50 @@ fn is_valid(digits: Vec<i32>) -> bool {
     }
     !decreasing && pair_equal
 }
+
+fn is_valid_part_two(digits: Vec<i32>) -> bool {
+    // Check that there is a set of exactly two digits the same
+    // And the digits never decrease
+
+    let mut prev_digit = 0;
+    let mut exactly_pair_equal = false;
+    let mut decreasing = false;
+    let mut sets = vec![];
+    let mut set = vec![];
+    for &digit in &digits {
+        if digit < prev_digit {
+            decreasing = true;
+            break;
+        }
+        if digit == prev_digit {
+            set.push(digit);
+        } else {
+            sets.push(set);
+            set = vec![digit];
+        }
+        prev_digit = digit;
+    }
+    sets.push(set);
+
+    for set in &sets {
+        if set.len() == 2 {
+            exactly_pair_equal = true;
+            break;
+        }
+    }
+
+    !decreasing && exactly_pair_equal
+}
+
+// if !decreasing {
+//     println!("hit");
+//     println!("{:?}", digits);
+//     println!("{:?}", sets);
+
+//     for set in &sets {
+//         if set.len() > 3 {
+//             println!("{:?}", digits);
+//             println!("{:?}", sets);
+//             break;
+//         }
+//     }
