717e6c3d45f9bece00525df4f90975d6f8a7c687
Will Grant <wpg23@cam.ac.uk>
2019-12-07 13:47:45 -0800

    amplifiers linked up (but broken)

--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -15,24 +15,53 @@ fn main() {
 fn run(filename: &str) -> Result<(), Box<dyn Error>> {
     // Read the input file
     let contents = fs::read_to_string(filename)?;
+    let contents = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0";
 
-    let contents = "3,225,1,225,6,6,1100,1,238,225,104,0,1102,46,47,225,2,122,130,224,101,-1998,224,224,4,224,1002,223,8,223,1001,224,6,224,1,224,223,223,1102,61,51,225,102,32,92,224,101,-800,224,224,4,224,1002,223,8,223,1001,224,1,224,1,223,224,223,1101,61,64,225,1001,118,25,224,101,-106,224,224,4,224,1002,223,8,223,101,1,224,224,1,224,223,223,1102,33,25,225,1102,73,67,224,101,-4891,224,224,4,224,1002,223,8,223,1001,224,4,224,1,224,223,223,1101,14,81,225,1102,17,74,225,1102,52,67,225,1101,94,27,225,101,71,39,224,101,-132,224,224,4,224,1002,223,8,223,101,5,224,224,1,224,223,223,1002,14,38,224,101,-1786,224,224,4,224,102,8,223,223,1001,224,2,224,1,223,224,223,1,65,126,224,1001,224,-128,224,4,224,1002,223,8,223,101,6,224,224,1,224,223,223,1101,81,40,224,1001,224,-121,224,4,224,102,8,223,223,101,4,224,224,1,223,224,223,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,1008,677,226,224,1002,223,2,223,1005,224,329,1001,223,1,223,107,677,677,224,102,2,223,223,1005,224,344,101,1,223,223,1107,677,677,224,102,2,223,223,1005,224,359,1001,223,1,223,1108,226,226,224,1002,223,2,223,1006,224,374,101,1,223,223,107,226,226,224,1002,223,2,223,1005,224,389,1001,223,1,223,108,226,226,224,1002,223,2,223,1005,224,404,1001,223,1,223,1008,677,677,224,1002,223,2,223,1006,224,419,1001,223,1,223,1107,677,226,224,102,2,223,223,1005,224,434,1001,223,1,223,108,226,677,224,102,2,223,223,1006,224,449,1001,223,1,223,8,677,226,224,102,2,223,223,1006,224,464,1001,223,1,223,1007,677,226,224,1002,223,2,223,1006,224,479,1001,223,1,223,1007,677,677,224,1002,223,2,223,1005,224,494,1001,223,1,223,1107,226,677,224,1002,223,2,223,1006,224,509,101,1,223,223,1108,226,677,224,102,2,223,223,1005,224,524,1001,223,1,223,7,226,226,224,102,2,223,223,1005,224,539,1001,223,1,223,8,677,677,224,1002,223,2,223,1005,224,554,101,1,223,223,107,677,226,224,102,2,223,223,1006,224,569,1001,223,1,223,7,226,677,224,1002,223,2,223,1005,224,584,1001,223,1,223,1008,226,226,224,1002,223,2,223,1006,224,599,101,1,223,223,1108,677,226,224,102,2,223,223,1006,224,614,101,1,223,223,7,677,226,224,102,2,223,223,1005,224,629,1001,223,1,223,8,226,677,224,1002,223,2,223,1006,224,644,101,1,223,223,1007,226,226,224,102,2,223,223,1005,224,659,101,1,223,223,108,677,677,224,1002,223,2,223,1006,224,674,1001,223,1,223,4,223,99,226";
     let instruction_set: Vec<i32> = contents
         .trim()
         .split(",")
         .map(|x| x.parse().unwrap())
         .collect();
 
-    println!("{:?}", instruction_set);
+    // Copy the instruction set to each Amplifier    
 
-    // Copy the instruction set to each Amplifier
-    let mut amplifier_a: Amplifier = Amplifier::new(instruction_set.clone());
 
-    let input_value = 5;
-    let phase_value = 0;
-    let output_value = amplifier_a.run_tape(input_value, phase_value);
+    // Vary the phase value from 0 to 4, inclusive, for each. Hold the max value
+    let mut max_output = 0;
+    let mut max_config = (0,0,0,0,0);
+    for a in 0..5 {
+        for b in 0..5 {
+            for c in 0..5 {
+                for d in 0..5 {
+                    for e in 0..5{
+                        let input_value = 0;
+                        let mut amplifier_a: Amplifier = Amplifier::new(instruction_set.clone());
+                        let mut amplifier_b: Amplifier = Amplifier::new(instruction_set.clone());
+                        let mut amplifier_c: Amplifier = Amplifier::new(instruction_set.clone());
+                        let mut amplifier_d: Amplifier = Amplifier::new(instruction_set.clone());
+                        let mut amplifier_e: Amplifier = Amplifier::new(instruction_set.clone());
+                        amplifier_a.run_tape(input_value, a);
+                        amplifier_b.run_tape(amplifier_a.output_signal.unwrap(), b);
+                        amplifier_c.run_tape(amplifier_b.output_signal.unwrap(), c);
+                        amplifier_d.run_tape(amplifier_c.output_signal.unwrap(), d);
+                        amplifier_e.run_tape(amplifier_d.output_signal.unwrap(), e);
+                    
+                        let output_signal = amplifier_e.output_signal.unwrap();
+                        if output_signal > max_output {
+                            max_output = output_signal;
+                            max_config = (a,b,c,d,e);
+                        }
+                    
 
-    // println!("{}", output_value);
+                    }
+                }
+            }
+        }
+    } 
+
+    println!("Largest possible output: {}", max_output );
+    println!("Config: {:?}", max_config );
+
 
     Ok(())
 }
@@ -173,44 +202,43 @@ fn get_digits(n: i32) -> Vec<i32> {
 #[derive(Debug)]
 struct Amplifier {
     memory: Vec<i32>,
-    input_signal: Option<i32>,
-    phase_signal: Option<i32>,
     output_signal: Option<i32>,
     cursor: usize,
 }
 
 impl Amplifier {
     fn new(memory: Vec<i32>) -> Amplifier {
-        let input_signal = None;
-        let phase_signal = None;
         let output_signal = None;
         let cursor: usize = 0;
         Amplifier {
             memory: memory,
-            input_signal: input_signal,
-            phase_signal: phase_signal,
             output_signal: output_signal,
             cursor,
         }
     }
 
     fn run_tape(&mut self, input_value: i32, phase_value: i32) -> () {
-        let mut phase_value_processed = true;
+        let mut phase_value_processed = false;
         loop {
             // Read the memory at the cursor position, and parse the opcode.
             println!("cursor position: {}", self.cursor);
             let instruction = Instruction::new(&self.memory, self.cursor);
             println!("{:?}", instruction);
             let prev_cursor = self.cursor;
-
             // We need a way to get the two inputs in when required, and extract the output.
-            if instruction.opcode == OpcodeKind::Input && !phase_value_processed {
-                self.process_instruction(&instruction, Some(phase_value));
-                phase_value_processed = true;
-            } else if instruction.opcode == OpcodeKind::Input && phase_value_processed {
-                self.process_instruction(&instruction, Some(input_value));
-            } else {
-                self.process_instruction(&instruction, None);
+            match instruction.opcode {
+                OpcodeKind::Input => {
+                    if !phase_value_processed {
+                        self.process_instruction(&instruction, Some(phase_value));
+                        phase_value_processed = true;
+                    } else {
+                        self.process_instruction(&instruction, Some(input_value));
+                    }
+                }
+                OpcodeKind::Exit => {
+                    break;
+                }
+                _ => self.process_instruction(&instruction, None),
             }
             if self.cursor == prev_cursor {
                 self.cursor += &instruction.parameters.len() + 1; // +1 to include the opcode
@@ -280,10 +308,12 @@ impl Amplifier {
                 if mode == 0 {
                     let position: usize = instruction.parameters[0] as usize;
                     let value = self.memory[position];
-                    println!("***********Instruction output: {}", value);
+                    self.output_signal = Some(value);
+                // println!("***********Instruction output: {}", value);
                 } else if mode == 1 {
                     let value = instruction.parameters[0];
-                    println!("***********Instruction output: {}", value);
+                    self.output_signal = Some(value);
+                    // println!("***********Instruction output: {}", value);
                 }
             }
             OpcodeKind::JumpIfTrue => {
