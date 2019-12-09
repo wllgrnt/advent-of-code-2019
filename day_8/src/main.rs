af85c8664ed40e230972b9ac401664938bf41bfd
Will Grant <wpg23@cam.ac.uk>
2019-12-08 17:00:04 -0800

    solution to part 2

--- day_8/src/main.rs
+++ day_8/src/main.rs
@@ -48,16 +48,45 @@ fn run(filename: &str) -> Result<(), Box<dyn Error>> {
         i += 1;
     }
 
-    println!("{}", min_zeros_index);
     let min_zeros_layer = &layers[min_zeros_index];
-    println!(
-        "num zeros: {}",
-        min_zeros_layer.iter().filter(|&n| *n == 0).count()
-    );
-    // We want the  the number of 1 digits multiplied by the number of 2 digits?
+    // We want the  the number of 1 digits multiplied by the number of 2 digits
     let num_ones = min_zeros_layer.iter().filter(|&n| *n == 1).count();
     let num_twos = min_zeros_layer.iter().filter(|&n| *n == 2).count();
     println!("num ones * num twos: {}", num_ones * num_twos);
 
+    // Part 2: Generate the image.  0 is black, 1 is white, and 2 is transparent.
+    let mut processed_image = vec![3; img_width * img_height];
+
+    for i in 0..processed_image.len() {
+        for layer in &layers {
+            let pixel_color = layer[i];
+            if pixel_color == 2 {
+                continue;
+            } else {
+                processed_image[i] = pixel_color;
+                break;
+            }
+        }
+        if processed_image[i] == 3 {
+            panic!("Something has gone wrong.");
+        }
+    }
+
+    // Now we have a processed image, with 0 black and 1 white.
+    // We have to convert it into a readable form.
+
+    for chunk in processed_image.chunks(img_width) {
+        let string: Vec<String> = chunk.into_iter().map(|d| process_digit(d)).collect();
+        println!("{}", string.join(" "));
+    }
+
     Ok(())
+}
+
+fn process_digit(&d: &u32) -> String {
+    if d == 1 {
+        "*".to_string()
+    } else {
+        " ".to_string()
+    }
 }
