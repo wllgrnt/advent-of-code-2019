97d65600477037f1252aed49fd68f63b9f0d882d
Will Grant <wpg23@cam.ac.uk>
2019-12-10 12:28:04 -0800

    working code for 10:1

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -28,28 +28,37 @@ fn run(filename: &str) -> Result<(), Box<dyn Error>> {
     // Put a mask on the asteroid position array that zeros every blocked position
 
     let asteroid_matrix = asteroid_matrix; // immutable
-    println!("{:?}", asteroid_matrix);
 
-    get_visible_asteroids(asteroid_matrix.clone(), 5, 3);
+    let mut max_number_of_seen_asteroids = 0;
+    let mut best_i = 0;
+    let mut best_j = 0;
+    for (i, row) in asteroid_matrix.iter().enumerate() {
+        for (j, value) in row.iter().enumerate() {
+            if *value == 1 {
+                let number_of_seen_asteroids = get_visible_asteroids(asteroid_matrix.clone(), i, j);
+                if number_of_seen_asteroids > max_number_of_seen_asteroids {
+                    max_number_of_seen_asteroids = number_of_seen_asteroids;
+                    best_i = i;
+                    best_j = j;
+                }
+            }
+        }
+    }
 
-    // let mut max_number_of_seen_asteroids = 0;
-    // let mut best_i = 0;
-    // let mut best_j = 0;
-    // for (i, row) in asteroid_matrix.iter().enumerate() {
-    //     for (j, value) in row.iter().enumerate() {
-    //         if *value == 1 {
-    //             let number_of_seen_asteroids = get_visible_asteroids(asteroid_matrix.clone(), i, j);
-    //             if number_of_seen_asteroids > max_number_of_seen_asteroids {
-    //                 max_number_of_seen_asteroids = number_of_seen_asteroids;
-    //                 best_i = i;
-    //                 best_j = j;
-    //             }
-    //         }
-    //         println!("At co-ord [{}, {}] is value: {}", i, j, value);
-    //     }
-    // }
+    println!(
+        "Top asteroid is at {}, {} and sees {} asteroids",
+        best_i, best_j, max_number_of_seen_asteroids
+    );
 
     Ok(())
+}
+
+fn gcd(m: i8, n: i8) -> i8 {
+    if m == 0 {
+        n.abs()
+    } else {
+        gcd(n % m, m)
+    }
 }
 
 fn get_visible_asteroids(
@@ -57,19 +66,92 @@ fn get_visible_asteroids(
     asteroid_i: usize,
     asteroid_j: usize,
 ) -> u32 {
-    // Spiral out from position asteroid_x, asteroid_y. If we spot an asteroid,
-    // Then zero out all the blocked  positions (by repeating the relative offset until we reach the edge)
-
-    // Iterate over rows, from row asteroid_i, asteroid_i+1, ... N then asteroid_i-1, asteroid_i-2, .. 0
+    /*  Spiral out from position asteroid_x, asteroid_y. If we spot an asteroid,
+        Then zero out all the blocked  positions (by repeating the relative offset until we reach the edge)
+        Iterate over rows, from row asteroid_i, asteroid_i+1, ... N then asteroid_i-1, asteroid_i-2, .. 0
+    */
+    // Check there is an asteroid at the specified position.
+    assert_eq!(asteroid_matrix[asteroid_i][asteroid_j], 1);
     let n = asteroid_matrix.len();
     let m = asteroid_matrix[0].len();
     for i in get_indices(asteroid_i, n) {
         for j in get_indices(asteroid_j, m) {
-            println!("{}, {}", i, j );
+            if i == asteroid_i && j == asteroid_j {
+                continue;
+            }
+
+            if asteroid_matrix[i][j] == 1 {
+                // Then calculate the offset between this asteroid and asteroid_i, asteroid_j.
+                // Set all blocked positions to zero.
+                hide_blocked_asteroids(&mut asteroid_matrix, asteroid_i, asteroid_j, i, j);
+            }
         }
-        println!("");
     }
-    0
+    // Now we should have gotten rid of all asteroids that can't be seen from (asteroid_i, asteroid_j).
+    // So just count the non-zero elements of the asteroid matrix (minus 1, for the asteroid itself)
+    count_nonzero(&asteroid_matrix)-1
+}
+
+fn count_nonzero(asteroid_matrix: &Vec<Vec<u8>>) -> u32 {
+    let mut nonzero = 0;
+    for row in asteroid_matrix.iter() {
+        for value in row.iter() {
+            if *value != 0 {
+                nonzero += 1;
+            }
+        }
+    }
+    nonzero
+}
+
+fn hide_blocked_asteroids(
+    asteroid_matrix: &mut Vec<Vec<u8>>,
+    asteroid_i: usize,
+    asteroid_j: usize,
+    i: usize,
+    j: usize,
+) -> () {
+    // If the asteroid is on the same row or column, zero the remaining row and column.
+    // Needs guard rails to stop accessing elements <0 or >m
+    let n = asteroid_matrix.len();
+    let m = asteroid_matrix[0].len();
+    if asteroid_i == i {
+        if j > asteroid_j && j < m {
+            for blocked_j in j + 1..m {
+                asteroid_matrix[i][blocked_j] = 0;
+            }
+        } else if j > 0 {
+            for blocked_j in 0..j - 1 {
+                asteroid_matrix[i][blocked_j] = 0;
+            }
+        }
+    } else if asteroid_j == j {
+        if i > asteroid_i && i < n {
+            for blocked_i in i + 1..n {
+                asteroid_matrix[blocked_i][j] = 0;
+            }
+        } else if i > 0 {
+            for blocked_i in 0..i - 1 {
+                asteroid_matrix[blocked_i][j] = 0;
+            }
+        }
+    }
+    // Otherwise, zero the positions at a repeat of the offset.
+    // If the offsets have a common factor, then simplify them.
+    // E.g. if the offset is (3,3) then everything (1,1) from there is blocked.
+    let mut offset_i: i8 = i as i8 - asteroid_i as i8;
+    let mut offset_j: i8 = j as i8 - asteroid_j as i8;
+    let gcd = gcd(offset_i.abs(), offset_j.abs());
+    offset_i /= gcd;
+    offset_j /= gcd;
+    // While within the limits of the asteroid matrix, zero things out
+    let mut blocked_i: i8 = i as i8 + offset_i;
+    let mut blocked_j: i8 = j as i8 + offset_j;
+    while blocked_i >= 0 && blocked_i < n as i8 && blocked_j >= 0 && blocked_j < m as i8 {
+        asteroid_matrix[blocked_i as usize][blocked_j as usize] = 0;
+        blocked_i += offset_i;
+        blocked_j += offset_j;
+    }
 }
 
 fn get_indices(index: usize, limit: usize) -> Vec<usize> {
